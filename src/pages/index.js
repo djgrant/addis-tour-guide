a95e702cef75f03daa1a888ed8e8db9bd792cf1d
Addis Tour Guide <110671638+addistourguide@users.noreply.github.com>
2026-03-15 13:59:25 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Card from '../components/atoms/card';
 
 export default () => (
   <Layout>
-    <Jumbotron image="IMG_2024111...701_618.jpg" position="middle" />
+    <Jumbotron image="IMG_2024111...701_618.jpg"position="middle" />
     <Box container gutter>
       <Article pt={[4, 4]} pb={[4, 5]}>
         <h2>Personal tour operator with a trusted independent guide</h2>
