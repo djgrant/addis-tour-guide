d422ede70934e9b0da9049c0a3adb59b148b8df0
Addis Tour Guide <110671638+addistourguide@users.noreply.github.com>
2026-03-17 17:01:41 +0300

    src/pages/tours/index.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import Layout from '../components/organisms/layout';
+import React from "react";
+import Layout from "../../components/organisms/layout";
 
-const NotFoundPage = () => (
-  <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
-  </Layout>
-);
+const TourPage = () => {
+  const params = new URLSearchParams(typeof window !== "undefined" ? window.location.search : "");
+  const tourName = params.get("tour");
 
-export default NotFoundPage;
+  const [content, setContent] = React.useState("");
+
+  React.useEffect(() => {
+    if (tourName) {
+      import(`../../tours/${tourName}.md`)
+        .then(module => fetch(module.default))
+        .then(res => res.text())
+        .then(text => setContent(text))
+        .catch(() => setContent("Tour not found"));
+    }
+  }, [tourName]);
+
+  return (
+    <Layout>
+      <div className="tour-page">
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
+    </Layout>
+  );
+};
+
+export default TourPage;
